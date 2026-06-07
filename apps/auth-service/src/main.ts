86463f5602c17ecaae126dc7ca28830eb3d2183d
RITHY Bondeth <rithybondeth999@gmail.com>
2026-06-07 21:32:05 +0700

    feat: integrate nestjs-pino for logging and refactor auth-service to use ConfigService for environment variables

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -2,19 +2,29 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AuthServiceModule } from './auth-service.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AuthServiceModule, {
-    transport: Transport.RMQ,
-    options: {
-      urls: [process.env.RABBITMQ_URL!],
-      queue: process.env.AUTH_QUEUE!,
-      queueOptions: {
-        durable: true,
+  const appContext =
+    await NestFactory.createApplicationContext(AuthServiceModule);
+  const configService = appContext.get<ConfigService>(ConfigService);
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthServiceModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [configService.get<string>('rabbitmq.url')!],
+        queue: configService.get<string>('rabbitmq.authQueue')!,
+        queueOptions: {
+          durable: true,
+        },
       },
     },
-  });
+  );
   await app.listen();
-  console.log(`Auth service is listening on queue ${process.env.AUTH_QUEUE}`);
+  console.log(
+    `Auth service is listening on queue ${configService.get<string>('rabbitmq.authQueue')}`,
+  );
 }
 bootstrap();
