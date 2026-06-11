c425baaf5a3be74484a5f18f92b7d1af2f184a0b
RITHY Bondeth <rithybondeth999@gmail.com>
2026-06-11 15:06:29 +0700

    feat: update user schema with authentication fields and add registration DTO

---
+++ libs/contracts/src/dtos/auth/register.dto.ts
@@ -0,0 +1,9 @@
+export class RegisterDTO {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  dateOfBirth: Date;
+  phone: string;
+}
--- libs/database/src/schemas/user/user.schema.ts
+++ libs/database/src/schemas/user/user.schema.ts
@@ -1,19 +1,72 @@
-import { boolean, integer, pgTable, text } from 'drizzle-orm/pg-core';
+import {
+  boolean,
+  integer,
+  pgTable,
+  text,
+  timestamp,
+} from 'drizzle-orm/pg-core';
 import { id } from '../common/id.schema';
 import { timestamps } from '../common/timestap.schema';
 
 export const user = pgTable('users', {
   ...id,
-  email: text('email').notNull().unique(),
-  password: text('password').notNull(),
+  // User Info
   firstName: text('first_name'),
   lastName: text('last_name'),
   dateOfBirth: text('date_of_birth'),
-  phone: text('phone'),
   avatar: text('avatar'),
   streak: integer('streak').default(0),
   xp: integer('xp').default(0),
-  isVerified: boolean('is_verified').default(false),
   isAdmin: boolean('is_admin').notNull().default(false),
+
+  // Email Password
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+
+  // Reset Password
+  resetPasswordToken: text('reset_password_token'),
+  resetPasswordTokenExpiresAt: timestamp('reset_password_token_expires_at', {
+    withTimezone: true,
+    mode: 'date',
+  }),
+
+  // Phone OTP
+  phone: text('phone'),
+  otpCode: text('otp_code'),
+  otpCodeExpiresAt: timestamp('otp_code_expires_at', {
+    withTimezone: true,
+    mode: 'date',
+  }),
+
+  // Email Verification
+  isEmailVerified: boolean('is_email_verified').default(false),
+  emailVerificationToken: text('email_verification_token'),
+  emailVerificationTokenExpiresAt: timestamp(
+    'email_verification_token_expires_at',
+    {
+      withTimezone: true,
+      mode: 'date',
+    },
+  ),
+
+  // Refresh Token
+  refreshToken: text('refresh_token'),
+  refreshTokenExpiresAt: timestamp('refresh_token_expires_at', {
+    withTimezone: true,
+    mode: 'date',
+  }),
+
+  // Social Login
+  googleId: text('google_id').unique(),
+  githubId: text('github_id').unique(),
+  facebookId: text('facebook_id').unique(),
+
+  // Last Login
+  lastLoginAt: timestamp('last_login_at', {
+    withTimezone: true,
+    mode: 'date',
+  }),
+  lastLoginMethod: text('last_login_method'),
+
   ...timestamps,
 });
